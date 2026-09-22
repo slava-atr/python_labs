9ae33e0f54d54c0d6c6d17a6f924656065c8d081
Слава <slava@MacBook-Air-M5.local>
2026-09-23 00:41:36 +0300

    Добавил функцию вычисления обратной польской нотации

--- lab_01/src/toolkit/calculator.py
+++ lab_01/src/toolkit/calculator.py
@@ -1,26 +1,11 @@
 import re
 
-from constants import TOKEN_PATTERN
+from constants import TOKEN_PATTERN, Stack
 
 
 def tokenize(expression):
     tokens = [match.group() for match in re.finditer(TOKEN_PATTERN, expression)]
     return tokens
-
-
-
-class Stack:
-    def __init__(self, *args):
-        self.items = []
-
-    def push(self, item):
-        self.items.append(item)
-
-    def pop(self):
-        return self.items.pop()
-
-    def is_empty(self):
-        return (self.items == [])
 
 
 def shunting_yard(tokens):
@@ -35,9 +20,9 @@ def shunting_yard(tokens):
         elif token == "(":
             operators.push(token)
         elif token == ")":
-            while not operators.is_empty() and operators.items() != "(":
+            while not operators.is_empty() and operators.items[-1] != "(":
                 output_queue.append(operators.pop())
-            if not operators.is_empty and operators.items[-1] == "(":
+            if not operators.is_empty() and operators.items[-1] == "(":
                 operators.pop()
         elif token in precedence:
             while (not operators.is_empty() and operators.items[-1] in precedence and \
@@ -49,3 +34,27 @@ def shunting_yard(tokens):
         output_queue.append(operators.pop())
 
     return output_queue
+
+
+def calculate_rpn(tokens):
+    numbers = Stack()
+
+    for token in tokens:
+        if token.isdigit():
+            numbers.push(float(token))
+        else:
+            second_number = numbers.pop()
+            first_number = numbers.pop()
+            
+            if token == "+":
+                result = first_number + second_number
+            elif token == "-":
+                result = first_number - second_number
+            elif token == "*":
+                result = first_number * second_number
+            elif token == "/":
+                result = first_number / second_number
+
+            numbers.push(result)
+
+    return numbers.pop()
--- lab_01/src/toolkit/constants.py
+++ lab_01/src/toolkit/constants.py
@@ -1,3 +1,16 @@
 import re
 
 TOKEN_PATTERN = re.compile(r"[-+]?\d+(?:\.\d+)?|[+\-*/()]")
+
+class Stack:
+    def __init__(self, *args):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def is_empty(self):
+        return (self.items == [])
